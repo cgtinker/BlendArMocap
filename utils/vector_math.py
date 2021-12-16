8a81a06a5beef3b809fcaabc02dca53a89a51830
dhsu <d.hsu@online.de>
2021-12-16 11:50:20 +0100

    rot center math

--- utils/vector_math.py
+++ utils/vector_math.py
@@ -2,32 +2,76 @@ import math
 from mathutils import Vector
 
 
-def get_vector_distance(v_a, v_b):
-    return math.sqrt(
-            (v_b[0] - v_a[0]) ** 2 +
-            (v_b[1] - v_a[1]) ** 2 +
-            (v_b[2] - v_a[2]) ** 2
-        )
-
-
+# region location
 def get_center_point(p_a, p_b):
+    """ return center point from two given points. """
     center_point = Vector((
-            (p_a[0] + p_b[0]) / 2,
-            (p_a[1] + p_b[1]) / 2,
-            (p_a[2] + p_b[2]) / 2,
-        ))
+        (p_a[0] + p_b[0]) / 2,
+        (p_a[1] + p_b[1]) / 2,
+        (p_a[2] + p_b[2]) / 2,
+    ))
     return center_point
 
 
-def get_vector_from_points(p_a, p_b):
-    """ p_a = origin; p_b = destination """
+def get_vector_from_points(origin, dest):
+    """ returns vector from two points. """
     vec = Vector((
-        p_b[0] - p_a[0],
-        p_b[1] - p_a[1],
-        p_b[2] - p_a[2]
+        dest[0] - origin[0],
+        dest[1] - origin[1],
+        dest[2] - origin[2]
     ))
     return vec
+# endregion
 
 
-def get_angle_between_vectors(v_a, v_b):
-    return v_a.angle(v_b)
+# region utils
+def get_direction_vec(vec_a, vec_b):
+    """ returns normalized direction vector. """
+    return Vector((
+        vec_a[0] - vec_b[0],
+        vec_a[1] - vec_b[1],
+        vec_a[2] - vec_b[2]
+    ))
+
+
+def get_vector_distance(p_a, p_b):
+    """ return distance between two points. """
+    return math.sqrt(
+        (p_b[0] - p_a[0]) ** 2 +
+        (p_b[1] - p_a[1]) ** 2 +
+        (p_b[2] - p_a[2]) ** 2
+    )
+
+
+def get_vector_length(vec):
+    """ returns the length of a given vector. """
+    return math.sqrt(
+        vec[0] ** 2 + vec[1] ** 2 + vec[2] ** 2
+    )
+
+
+def normalize_vector(vec):
+    """ returns a normalized vector. """
+    magnitude = get_vector_length(vec)
+    return Vector((
+        vec[0] / magnitude,
+        vec[1] / magnitude,
+        vec[2] / magnitude
+    ))
+# endregion
+
+
+# region Rotation
+def rotate_towards(origin, destination):
+    """ returns rotation from an origin to a destination. """
+    dir_vec = get_direction_vec(origin, destination)
+    dir_vec = dir_vec.normalized()
+    quart = dir_vec.to_track_quat('Z', 'Y')
+    euler = quart.to_euler('XYZ')
+    return euler
+
+
+def get_angle_between_vectors(vec_a, vec_b):
+    """ returns angle between two vectors. """
+    return vec_a.angle(vec_b)
+# endregion
