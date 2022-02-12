868bce5b248f4f56ea829cf7c55514c0693510f8
dhsu <d.hsu@online.de>
2022-02-12 18:48:38 +0100

    pathlib

--- cgt_imports.py
+++ cgt_imports.py
@@ -1,54 +1,58 @@
 import importlib
-import os
 import sys
+from pathlib import Path
 
 """
 This makes sure all modules are reloaded from new files, when the addon is removed and a new version is installed in the same session,
 or when Blender's 'Reload Scripts' operator is run manually.
 """
+
 SUB_DIRS = ['cgt_blender', 'cgt_bridge', 'cgt_detection', 'cgt_utils']
 INIT_MODULES = [
-    'cgt_naming',
-    'cgt_imports',
-    'cgt_blender.interface.ui_properties',
-    'cgt_blender.interface.ui_registration',
-    'cgt_blender.input_manager',
-    'cgt_blender.interface',
-    'cgt_blender.utils.install_dependencies',
+    '.cgt_naming',
+    '.cgt_imports',
+    '.cgt_blender.interface.ui_properties',
+    '.cgt_blender.interface.ui_registration',
+    '.cgt_blender.input_manager',
+    '.cgt_blender.interface',
+    '.cgt_blender.utils.install_dependencies',
 ]
 
-PACKAGE = os.path.basename(os.path.dirname(__file__))
+FILE = Path(__file__)
+PARENT = FILE.parent
+PACKAGE = PARENT.name
 
 
 def import_module(module):
-    print(f"importing {PACKAGE}.{module}...")
-    importlib.import_module(f"{PACKAGE}.{module}")
+    importlib.import_module(f"{PACKAGE}{module}")
 
 
 def reload_module(module):
-    print(f"reloading {PACKAGE}.{module}...")
-    importlib.reload(sys.modules[f"{PACKAGE}.{module}"])
+    importlib.reload(sys.modules[f"{PACKAGE}{module}"])
 
 
 def get_reload_list(sub_dirs):
     reload_list = []
 
     for sub_dir in sub_dirs:
-        for root, sub_dir, files in os.walk(sub_dir):
-            # get all python modules files for the import
-            sub_path = root.replace(os.path.dirname(__file__) + "/", "")
-            sub_path = sub_path.replace("/", ".")
-            modules = [f"{file.replace('.py', '')}" for file in files
-                       if file.endswith('.py') if file != '__init__.py']
-
-            for module in modules:
-                m_module = f"{sub_path}.{module}"
-                reload_list.append(m_module)
-
+        files = [p for p in sub_dir.rglob("*.py") if p.stem != '__init__']
+        for file in files:
+            parents = get_parents(file, [])
+            imp_path = ""
+            for parent in reversed(parents):
+                imp_path += f".{parent}"
+            imp_path += f".{file.stem}"
+            reload_list.append(imp_path)
     return reload_list
 
 
-# first reload the required modules to activate the UI
+def get_parents(file: Path, parents: list):
+    if file.parent.name != PACKAGE:
+        parents.append(file.parent.name)
+        get_parents(file.parent, parents)
+    return parents
+
+
 def manage_imports(reload=False):
     for module in INIT_MODULES:
         import_module(module)
@@ -58,12 +62,12 @@ def manage_imports(reload=False):
 
     if install_dependencies.dependencies_installed is True:
         print(f"Attempt to reload {PACKAGE}")
-        package = os.path.dirname(__file__)
-        sub_dirs = [os.path.join(package, sub_dir) for sub_dir in SUB_DIRS]
+        sub_dirs = [PARENT / sub_dir for sub_dir in SUB_DIRS]
         reload_list = get_reload_list(sub_dirs)
 
         for module in reload_list:
             if reload is True:
+                import_module(module)
                 reload_module(module)
             else:
                 import_module(module)
