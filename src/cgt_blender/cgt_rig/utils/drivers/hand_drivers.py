54884d1ceef0a010244bfcf468c18e389a1f4a2d
dhsu <d.hsu@online.de>
2022-03-10 01:54:04 +0100

    implemented z slopes

--- src/cgt_blender/cgt_rig/utils/drivers/hand_drivers.py
+++ src/cgt_blender/cgt_rig/utils/drivers/hand_drivers.py
@@ -22,14 +22,11 @@ class FingerAngleDriver(DriverProperties):
     target_object: str
     functions: list
 
-    # def __init__(self, driver_target, provider_obj, factor, offset):
     def __init__(self,
                  driver_target: str,
                  provider_obj: object,
-                 slope: Slope,
-                 z_offset: float,
-                 z_dir: int,
-                 z_factor: float):
+                 x_slope: Slope,
+                 z_slope: Slope):
         """ Provides eye driver properties to animate the lids.
             :param provider_obj: object providing rotation values.
             :param slope: factor to multiply and offset the rotation
@@ -42,86 +39,74 @@ class FingerAngleDriver(DriverProperties):
         self.property_type = "rotation_euler"
         self.property_name = "rotation"
         self.data_paths = ["rotation_euler[0]", "rotation_euler[1]", "rotation_euler[2]"]
-        self.functions = [f"{slope.min_out})+{slope.slope}*({slope.min_in}+",
+        self.functions = [f"{x_slope.min_out})+{x_slope.slope}*({x_slope.min_in}+",
                           "",
-                          # z_func]
-                          f"{z_offset} + {z_dir} * rotation * {z_factor} if rotation != 0 else"]
+                          f"{z_slope.min_out})+{z_slope.slope}*({z_slope.min_in}+"]
         self.overwrite = True
 
 
 @dataclass(repr=True)
 class FingerDriverContainer(DriverContainer):
-    # approx rounded input min max values based on 500 sample values
-    # L / R hand inverted
-    # todo: setup slopes
-    z_factor = 5.5
-    z_angle_offset = [
-        -.161, 0, 0,
-        0.534, 0, 0,
-        0.114, 0, 0,
-        -.361, 0, 0,
-        -.376, 0, 0,
+    z_inputs = [
+        [0.0, 16],  [0, 1], [0, 1],  # thumb
+        [0.0, 7.5], [0, 1], [0, 1],  # index
+        [0.0, 4.0], [0, 1], [0, 1],  # middle
+        [0.0, 5.8], [0, 1], [0, 1],  # ring
+        [0.0, 6],   [0, 1], [0, 1],  # pinky
     ]
 
-    min_input = [
-        0.22, 0.80, 0.59,
-        5.06, 0.66, 1.17,
-        1.11, 0.98, 0.41,
-        0.62, 1.62, 0.05,
-        5.21, 2.30, 0.71
+    z_outputs = [
+        [-20, 20], [0, 1], [0, 1],  # thumb
+        [9.5, -18], [0, 1], [0, 1],  # index
+        [-9, 7], [0, 1], [0, 1],  # middle
+        [-5, 12], [0, 1], [0, 1],  # ring
+        [-10, 20], [0, 1], [0, 1],  # pinky
     ]
 
-    max_input = [
-        42.76, 58.19, 93.19,  # thumb
-        117.0, 116.3, 86.71,  # index
-        104.8, 112.4, 111.0,  # middle
-        121.3, 108.2, 99.16,  # ring
-        123.9, 95.04, 117.8  # pinky
+    x_inputs = [
+        [0.22, 42.76], [0.80, 58.19], [0.59, 93.19],  # thumb
+        [5.06, 117.0], [0.66, 116.3], [1.17, 86.71],  # index
+        [1.11, 104.8], [0.98, 112.4], [0.41, 111.0],  # middle
+        [0.62, 121.3], [1.62, 108.2], [0.05, 99.16],  # ring
+        [5.21, 123.9], [2.30, 95.04], [0.71, 117.8]  # pinky
     ]
 
-    min_output = [
-        -47.5, -17.5, -12.5,  # thumb
-        -62.5, -32.5, -87.5,  # index
-        -37.5, -62.5, -12.5,  # middle
-        -42.5, -32.5, -27.5,  # ring
-        -82.5, -60.0, -37.5,  # pinky
+    x_outputs = [
+        [-47.5, 42.76], [-17.5, 58.19], [-12.5, 93.19],  # thumb
+        [-62.5, 117.0], [-32.5, 116.3], [-87.5, 86.71],  # index
+        [-37.5, 104.8], [-62.5, 112.4], [-12.5, 111.0],  # middle
+        [-42.5, 121.3], [-32.5, 108.2], [-27.5, 99.16],  # ring
+        [-82.5, 123.9], [-60.0, 95.04], [-37.5, 117.8]  # pinky
     ]
-
-    max_output = [
-        42.76, 58.19, 93.19,  # thumb
-        117.0, 116.3, 86.71,  # index
-        104.8, 112.4, 111.0,  # middle
-        121.3, 108.2, 99.16,  # ring
-        123.9, 95.04, 117.8  # pinky
-    ]
-
-    limits = [0.00] * 15
 
     def __init__(self, driver_targets: list, provider_objs: list, orientation: str):
-        # slopes = [Slope(0, 1, 0, 1)
-        #           for idx, _ in enumerate(self.max_input)]
-        slopes = [Slope(self.min_input[idx], self.max_input[idx], self.min_output[idx], self.max_output[idx])
-                  for idx, _ in enumerate(self.max_input)]
+        x_slopes = [
+            Slope(self.x_inputs[idx][0], self.x_inputs[idx][1], self.x_outputs[idx][0], self.x_outputs[idx][1])
+            for idx in range(0, 15)
+        ]
 
-        def z_angle_function(idx):
-            offset = 0
-            if orientation == "left":
-                offset = self.z_angle_offset[idx]
-            elif orientation == "right":
-                offset = self.z_angle_offset[idx] * -1
+        z_slopes = [
+            Slope(self.z_inputs[idx][0], self.z_inputs[idx][1], self.z_outputs[idx][0], self.z_outputs[idx][1])
+            for idx in range(0, 15)
+        ]
 
-            if offset > 0:
-                factor = -1
+        # values have to be mirrored to fit angles
+        self.z_outputs = [[i[0]*-1, i[1]*-1] for i in self.z_outputs]
+        m_slopes = [
+            Slope(self.z_inputs[idx][0], self.z_inputs[idx][1], self.z_outputs[idx][0], self.z_outputs[idx][1])
+            for idx in range(0, 15)
+        ]
+
+        def get_z_slope(idx):
+            if orientation == "right":
+                return z_slopes[idx]
             else:
-                factor = 1
-            return offset, factor
+                return m_slopes[idx]
 
         self.pose_drivers = [
             FingerAngleDriver(
                 driver_targets[idx],
                 provider_objs[idx],
-                slopes[idx],
-                z_angle_function(idx)[0],
-                z_angle_function(idx)[1],
-                self.z_factor
-        ) for idx, _ in enumerate(driver_targets)]
+                x_slopes[idx],
+                get_z_slope(idx),
+            ) for idx, _ in enumerate(driver_targets)]
